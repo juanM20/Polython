7fb300c0f79879ac23ce9e767a16b896c26b9b01
Charly OG <caog.freeman@gmail.com>
2019-03-02 17:11:31 -0600

    A ver, prueba el footer Juanjo

---
+++ test/footer-p.js
@@ -0,0 +1,53 @@
+import {html, PolymerElement} from '@polymer/polymer/polymer-element.js';
+
+/**
+ * `menu-p`
+ * Menu_de_navegacion
+ *
+ * @customElement
+ * @polymer
+ * @demo demo/index.html
+ */
+class FooterP extends PolymerElement {
+  static get template() {
+    return html`
+      <style>
+        :host {
+          display: block;
+        }
+
+        .footer {
+          position: absolute;
+          left: 0;
+          right: 0;
+          bottom: 0;
+          padding: 0;
+          background-color: : #000080;
+        }
+
+        .footer p {
+          font-family: sans-serif;
+          font-size: 2em;
+          color: #FFFFFF;
+        }
+      </style>
+
+      <footer>
+        <p>Desarrollado por Qubits</p>
+        <p>Polython 2019</p>
+        <p>Ciudad de México</p>
+      </footer>
+    `;
+  }
+
+  static get properties() {
+    return {
+      prop1: {
+        type: String,
+        value: 'footer-p',
+      },
+    };
+  }
+}
+
+window.customElements.define('footer-p', FooterP);
