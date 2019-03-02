772c1110c5e06068f8c387c889d6e3e9efa41780
juanM20 <jmtp12poke@gmail.com>
2019-03-02 14:04:08 -0600

    primer_elemento

---
+++ menu-p.js
@@ -0,0 +1,32 @@
+import {html, PolymerElement} from '@polymer/polymer/polymer-element.js';
+
+/**
+ * `menu-p`
+ * Menu_de_navegacion
+ *
+ * @customElement
+ * @polymer
+ * @demo demo/index.html
+ */
+class MenuP extends PolymerElement {
+  static get template() {
+    return html`
+      <style>
+        :host {
+          display: block;
+        }
+      </style>
+      <h2>Hello [[prop1]]!</h2>
+    `;
+  }
+  static get properties() {
+    return {
+      prop1: {
+        type: String,
+        value: 'menu-p',
+      },
+    };
+  }
+}
+
+window.customElements.define('menu-p', MenuP);
