52c535892e872dc3378d202e63a2521a3a7ea34b
Charly OG <caog.freeman@gmail.com>
2019-03-03 06:16:07 -0600

    prueba 2

--- prueba.js
+++ prueba.js
@@ -1,7 +1,26 @@
-const database = firebase.database();
+/*const database = firebase.database();
 
 let refData = database.ref();
 
 refData.once('value', snapshot => {
+  console.log(snapshot.val());
+});
+*/
+
+const data = firebase.database();
+let data = {
+  "Moscow": {
+    country: "Russia"
+  },
+  "Berlin": {
+    name: "Germany"
+  }
+}
+Referenciamos al nodo principal
+let dataRef = database.ref('cities');
+Pusheamos los datos al nodo
+let dataPush = dataRef.push(data);
+Visualizamos los datos al nodo
+dataRef.once('value', snapshot => {
   console.log(snapshot.val());
 });
